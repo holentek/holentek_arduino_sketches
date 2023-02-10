c9b2433c37179d4b714893b7b2803fe20967ce4d
M. Holen <53666103+holentek@users.noreply.github.com>
2023-02-10 19:21:20 +0100

    Create nightrider.ino

---
+++ nightrider/nightrider.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_NeoPixel.h>
+
+// Define the number of LEDs and the data pin for the WS2812 strip
+#define LED_COUNT 16
+#define LED_PIN 4
+
+// Create an instance of the Adafruit NeoPixel library
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  // Initialize the NeoPixel library
+  strip.begin();
+  strip.show();
+}
+
+void loop() {
+  // Night Rider animation
+  for (int i = 0; i < LED_COUNT; i++) {
+    // Turn on the current LED
+    strip.setPixelColor(i, 255, 0, 0);
+    strip.show();
+    // Turn off the previous LED
+    if (i > 0) {
+      strip.setPixelColor(i-1, 0, 0, 0);
+    }
+    // Delay between LEDs
+    delay(100);
+  }
+
+  // Repeat the animation in the reverse direction
+  for (int i = LED_COUNT-1; i >= 0; i--) {
+    // Turn on the current LED
+    strip.setPixelColor(i, 255, 0, 0);
+    strip.show();
+    // Turn off the previous LED
+    if (i < LED_COUNT-1) {
+      strip.setPixelColor(i+1, 0, 0, 0);
+    }
+    // Delay between LEDs
+    delay(100);
+  }
+}
